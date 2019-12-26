d05d63020873894df9c759429fce5c0b96cf80a3
Trey Foster <treybfoster@gmail.com>
2019-12-26 15:00:53 -0600

    update

--- dist/index.js
+++ dist/index.js
@@ -8,83 +8,42 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var axios_1 = require("axios");
-var jsdom_1 = require("jsdom");
-var isBrowser = typeof DOMParser !== "undefined";
-var BibleGatewayAPI = (function () {
-    function BibleGatewayAPI() {
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const jsdom_1 = require("jsdom");
+const isBrowser = typeof DOMParser !== "undefined";
+class BibleGatewayAPI {
+    constructor() {
         this.parse = null;
         if (isBrowser) {
-            this.parse = function (content) {
-                return new DOMParser().parseFromString(content, "text/html");
-            };
+            this.parse = (content) => new DOMParser().parseFromString(content, "text/html");
         }
         else {
-            this.parse = function (content) {
-                var document = new jsdom_1.JSDOM(content).window.document;
+            this.parse = (content) => {
+                const { document } = new jsdom_1.JSDOM(content).window;
                 return document;
             };
         }
     }
-    BibleGatewayAPI.prototype.search = function (query, version) {
-        if (query === void 0) { query = "John 3:16"; }
-        if (version === void 0) { version = "ESV"; }
-        return __awaiter(this, void 0, void 0, function () {
-            var encodedSearch, encoodedVersion, url, result, document, verse, elements, content, i, text;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        encodedSearch = encodeURIComponent(query);
-                        encoodedVersion = encodeURIComponent(version);
-                        url = "https://www.biblegateway.com/passage?search=" + encodedSearch + "&version=" + encoodedVersion;
-                        return [4, axios_1["default"].get(url)];
-                    case 1:
-                        result = _a.sent();
-                        document = this.parse(result.data);
-                        verse = document.querySelector(".bcv").textContent;
-                        elements = [].slice.call(document.querySelectorAll(".text"));
-                        content = [];
-                        for (i = 0; i < elements.length; i++) {
-                            text = elements[i].textContent;
-                            if (text.substr(0, 4) != "Back")
-                                content.push(text);
-                        }
-                        if (content.length === 0)
-                            throw new Error("Could not find verse");
-                        return [2, Promise.resolve({ verse: verse, content: content })];
-                }
-            });
+    search(query = "John 3:16", version = "ESV") {
+        return __awaiter(this, void 0, void 0, function* () {
+            let encodedSearch = encodeURIComponent(query);
+            let encoodedVersion = encodeURIComponent(version);
+            const url = `https://www.biblegateway.com/passage?search=${encodedSearch}&version=${encoodedVersion}`;
+            const result = yield axios_1.default.get(url);
+            const document = this.parse(result.data);
+            const verse = document.querySelector(".bcv").textContent;
+            let elements = [].slice.call(document.querySelectorAll(".text"));
+            let content = [];
+            for (let i = 0; i < elements.length; i++) {
+                let text = elements[i].textContent;
+                if (text.substr(0, 4) != "Back")
+                    content.push(text);
+            }
+            if (content.length === 0)
+                throw new Error("Could not find verse");
+            return Promise.resolve({ verse, content });
         });
-    };
-    return BibleGatewayAPI;
-}());
+    }
+}
 exports.BibleGatewayAPI = BibleGatewayAPI;
-//# sourceMappingURL=index.js.map
